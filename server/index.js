3c9010a7e15f97347578b561c92aff6c0f308d4f
volinhkhanh <vo.khanh1104@gmail.com>
2022-03-29 17:00:28 +0700

    intergrate redux and create form

--- server/index.js
+++ server/index.js
@@ -1,4 +1,32 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+import postRoutes from "./routes/posts.js";
+
+const app = express();
+
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use("/posts", postRoutes);
+
+const CONNECTION_URL =
+  "mongodb+srv://khanh:khanh2005@cluster0.mes8t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port : ${PORT}`))
+  )
+  .catch((error) => console.log(error, message));
+
+// mongoose.set("useFindAndModify", false);
+//https://www.mongodb.com/cloud/atlas
---
+++ server/routes/posts.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+import { getPosts, createPosts } from "../controllers/posts.js";
+
+const router = express.Router();
+
+// http://localhost:5000/posts
+
+router.get("/", getPosts);
+router.get("/", createPosts);
+
+export default router;
